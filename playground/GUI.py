4018ce2e1248823111da23c4b149041b47e2993e
rkiresich <rkiresich@gmail.com>
2019-03-01 16:13:41 -0700

    updated GUI
    Simulates live data, clean abort close button

--- playground/GUI.py
+++ playground/GUI.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Frame, BOTH, LEFT, RIGHT, Label, ttk, TOP, NE, BOTTOM
+from tkinter import Frame, BOTH, LEFT, RIGHT, Label, ttk, TOP, NE, BOTTOM, Message, Button
 from tkinter.ttk import Style
 import TLMdisplayParse
 
@@ -9,15 +9,32 @@ root = tk.Tk()
 style = tk.ttk.Style()
 style.theme_use('classic')
 
-mainFrame = Frame(root, height=300, width=1300)
+mainFrame = Frame(root, height=350, width=1300)
 mainFrame.pack(fill=BOTH, expand=tk.YES)
 mainFrame.pack_propagate(0)
 
 vidFrame = Frame(mainFrame, height=300, width=650, bg="#607674")
 vidFrame.pack(side=LEFT,fill=BOTH, expand=tk.YES)
+vidFrame.pack_propagate(0)
 
-tlmFrame = Frame(mainFrame, height=300,width=650, bg="blue")
+tlmFrame = Frame(mainFrame, height=300,width=650, bg="black")
 tlmFrame.pack(side=RIGHT,fill=BOTH, expand=tk.YES)
+tlmFrame.pack_propagate(0)
+
+closeInd = 0
+index = 0
+
+def closeDownSet(x):
+    global closeInd
+    closeInd = x
+
+
+def closeDownRead():
+    global closeInd
+    return closeInd
+
+exitButton = Button(root, text="ABORT", command = lambda: closeDownSet(1))
+exitButton.pack(side=BOTTOM, fill=BOTH, expand=tk.YES)
 
 dataFrame1 = Frame(tlmFrame, height=150, width=650, bg = "white")
 dataFrame1.pack(side = TOP,fill=BOTH, expand=tk.YES)
@@ -32,44 +49,56 @@ altFrame.pack(side = LEFT,fill=BOTH, expand=tk.YES)
 altFrame.pack_propagate(0)
 altLabel = Label(altFrame, text=tlmCategories[0], fg="white", bg="black", anchor=NE)
 altLabel.pack(side=LEFT, fill=tk.Y)
+altMsg = Message(altFrame)
 
 tempFrame = Frame(dataFrame1, height=150, width=325, bg = "black")
 tempFrame.pack(side = RIGHT,fill=BOTH, expand=tk.YES)
 tempFrame.pack_propagate(0)
 tempLabel = Label(tempFrame, text=tlmCategories[1], fg="white", bg="black", anchor=NE)
 tempLabel.pack(side=LEFT, fill=tk.Y)
+tempMsg = Message(tempFrame)
 
 pwrFrame = Frame(dataFrame2, height=150, width=325, bg = "black")
 pwrFrame.pack(side = LEFT,fill=BOTH, expand=tk.YES)
 pwrFrame.pack_propagate(0)
 pwrLabel = Label(pwrFrame, text=tlmCategories[2], fg="white", bg = "black", anchor=NE)
 pwrLabel.pack(side=LEFT, fill=tk.Y)
+pwrMsg = Message(pwrFrame)
+pwrMsg2 = Message(pwrFrame)
 
 hlthFrame = Frame(dataFrame2, height=150, width=325, bg = "black")
 hlthFrame.pack(side = RIGHT,fill=BOTH, expand=tk.YES)
 hlthFrame.pack_propagate(0)
 hlthLabel = Label(hlthFrame, text=tlmCategories[3], fg="white", bg="black", anchor=NE)
 hlthLabel.pack(side=LEFT, fill=tk.Y)
+hlthMsg = Message(hlthFrame)
+hlthMsg2 = Message(hlthFrame)
 
-index = 0
 
 def refreshData(index):
-    altObj = TLMdisplayParse.TLMdisplayParse(altFrame,index,6)
-    tempObj = TLMdisplayParse.TLMdisplayParse(tempFrame,index,1)
-    pwrObj1 = TLMdisplayParse.TLMdisplayParse(pwrFrame,index,21)
-    pwrObj2 = TLMdisplayParse.TLMdisplayParse(pwrFrame,index,20)
-    hlthObj1 = TLMdisplayParse.TLMdisplayParse(hlthFrame,index,38)
-    hlthObj2 = TLMdisplayParse.TLMdisplayParse(hlthFrame,index,41)
+    
+    altObj = TLMdisplayParse.TLMdisplayParse(altMsg,index,6)
+    tempObj = TLMdisplayParse.TLMdisplayParse(tempMsg,index,1)
+    pwrObj1 = TLMdisplayParse.TLMdisplayParse(pwrMsg,index,21)
+    pwrObj2 = TLMdisplayParse.TLMdisplayParse(pwrMsg2,index,20)
+    hlthObj1 = TLMdisplayParse.TLMdisplayParse(hlthMsg,index,38)
+    hlthObj2 = TLMdisplayParse.TLMdisplayParse(hlthMsg2,index,41)
     root.update()
-    del altObj
-    del tempObj
-    del pwrObj1
-    del pwrObj2
-    del hlthObj1
-    del hlthObj2
+    root.after(50)
 
-while index < 20:
+
+while(index < 5084):
+
+    shouldExit = closeDownRead()
+    if(shouldExit == 1):
+        break
+    
     refreshData(index)
     index += 1
+    
+    if(index == 4999):
+        index = 0
 
+    
+root.destroy()
 root.mainloop()
--- playground/TLMdisplayParse.py
+++ playground/TLMdisplayParse.py
@@ -8,8 +8,8 @@ class TLMdisplayParse(object):
     obj = TLMFileRead.TLMFileRead()
     arr = obj.fileRead('data/testData.txt')
 
-    def __init__(self, tlmframe, index, case):
-        self.tlmframe = tlmframe
+    def __init__(self, msgframe, index, case):
+        self.msgframe = msgframe
         self.index = index
         self.case = case
         self.display()
@@ -21,45 +21,45 @@ class TLMdisplayParse(object):
         
         if(self.case == 6):
             
-            msg = tk.Label(self.tlmframe, textvariable=var, font=tlmFont,
-                             bg="black", fg="white",
-                             pady=65, width=150)
-            msg.pack(side=tk.TOP, fill=tk.BOTH)
+            self.msgframe.configure(textvariable=var, font=tlmFont,
+                                     bg="black", fg="white",
+                                     pady=65, width=150)
+            self.msgframe.pack(side=tk.TOP, fill=tk.BOTH)
             var.set(self.arr[self.case][self.index] + " ft")
         if(self.case == 1):
             
-            msg = tk.Label(self.tlmframe, textvariable=var, font=tlmFont,
-                             bg="black", fg="white",
-                             pady=65, width=150)
-            msg.pack(side=tk.TOP, fill=tk.BOTH)
+            self.msgframe.configure(textvariable=var, font=tlmFont,
+                                     bg="black", fg="white",
+                                     pady=65, width=150)
+            self.msgframe.pack(side=tk.TOP, fill=tk.BOTH)
             var.set(self.arr[self.case][self.index] + " deg F")
         if(self.case == 21):
             
-            msg = tk.Label(self.tlmframe, textvariable=var, font=tlmFont,
-                             bg="black", fg="white",
-                             pady=25, width=150)
-            msg.pack(side=tk.TOP, fill=tk.BOTH)
+            self.msgframe.configure(textvariable=var, font=tlmFont,
+                                     bg="black", fg="white",
+                                     pady=25, width=150)
+            self.msgframe.pack(side=tk.TOP, fill=tk.BOTH)
             var.set(self.arr[self.case][self.index] + " V")
         if(self.case == 20):
             
-            msg = tk.Label(self.tlmframe, textvariable=var, font=tlmFont,
-                             bg="black", fg="white",
-                             pady=25, width=150)
-            msg.pack(side=tk.BOTTOM, fill=tk.BOTH)
+            self.msgframe.configure(textvariable=var, font=tlmFont,
+                                     bg="black", fg="white",
+                                     pady=25, width=150)
+            self.msgframe.pack(side=tk.BOTTOM, fill=tk.BOTH)
             var.set(self.arr[self.case][self.index] + "%")
         if(self.case == 38):
             
-            msg = tk.Label(self.tlmframe, textvariable=var, font=tlmFont,
-                             bg="black", fg="white",
-                             pady=25, width=150)
-            msg.pack(side=tk.TOP, fill=tk.BOTH)
+            self.msgframe.configure(textvariable=var, font=tlmFont,
+                                     bg="black", fg="white",
+                                     pady=25, width=150)
+            self.msgframe.pack(side=tk.TOP, fill=tk.BOTH)
             var.set(self.arr[self.case][self.index] + "?")
         if(self.case == 41):
             
-            msg = tk.Label(self.tlmframe, textvariable=var, font=tlmFont,
-                             bg="black", fg="white",
-                             pady=25, width=150)
-            msg.pack(side=tk.BOTTOM, fill=tk.BOTH)
+            self.msgframe.configure(textvariable=var, font=tlmFont,
+                                     bg="black", fg="white",
+                                     pady=25, width=150)
+            self.msgframe.pack(side=tk.BOTTOM, fill=tk.BOTH)
             var.set(self.arr[self.case][self.index] + "?")
 
 
